033836a80c17ee716fe751381ebb1da78f180f3e
Bassil <116284488+Bassil88@users.noreply.github.com>
2023-10-28 10:15:50 +0200

    Arithmetic Operators in c

---
+++ arithmetic-operators.c
@@ -0,0 +1,50 @@
+
+//Arithmetic Operators:
+int a = 10, b = 3;
+int sum = a + b; // Addition
+int difference = a - b; // Subtraction
+int product = a * b; // Multiplication
+int quotient = a / b; // Division
+int remainder = a % b; // Modulo (remainder after division)
+
+
+//Relational Operators:
+//Used to compare values. Common operators include Equal to (==), Not equal to (!=), Greater than (>), Less than (<), Greater than or equal to (>=), Less than or equal to (<=).
+int x = 5, y = 10;
+if (x > y) {
+    // Perform some action if x is greater than y
+}
+
+
+
+//Logical Operators:
+//Used for logical operations. Common logical operators include AND (&&), OR (||), and NOT (!).
+
+//Operand 1	Operand 2	Result
+//True	&& True=	True
+//True	&& False=	False
+//False	&& True=	False
+//False	&& False=	False
+//The && operator returns true only if both operands are true. Otherwise, it returns false.
+
+
+//Operand 1	Operand 2	Result
+//True	|| True=	True
+//True	|| False=	True
+//False	|| True=	True
+//False	|| False=	False
+//The || operator returns true if at least one of the operands is true. It returns false only if both operands are false.
+int p = 1, q = 0;
+if (p && q) {
+    // This condition is true only if both p and q are true (non-zero)
+}
+
+
+
+//Assignment Operators:
+//++ (Increment) and -- (Decrement) operators used to increase or decrease the value of a variable by 1.
+int count = 10;
+count++; // Increment count by 1
+
+
+//
