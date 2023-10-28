748aceb0e8d1edf0d54e4074300475eb835ae78a
Bassil <116284488+Bassil88@users.noreply.github.com>
2023-10-28 11:45:14 +0200

    loop kinds and use cases of them

---
+++ diff-loops.c
@@ -0,0 +1,52 @@
+
+//this is what we call an infinite loop. the lines of code between the curly braces will execute repeatedly from top to botttom, 
+//until and unless we break out of it (as with a break; statement) or otherwise kill out program.
+//if the Boolean expression inside of the while loop evaluates to true, the lines of code between the curly braces
+//will execute repeatedly over and over and over until Boolean expression evaluates to false.
+// Somewhat confusinly. the behavior of the Scratch block is reversed, but it is the closest analog
+while (boolean-expr) {}
+
+
+
+//other kind of Loop is
+
+//This loop will execute all the lines of code between the curly braces once, and then it will check the Boolean expression.
+//If the Boolean expression evaluates to true, will go back and repeat that process over and over and over until the Boolean expression evaluates to false.
+
+//So this loop, unlike a while loop, is guaranteed to run at least one time.
+do
+{
+  
+}
+  while (boolean-expr);
+
+
+
+
+
+//Lastly, we have a for loop.
+for (int i = 0; i < 10; i++)
+
+  //For loops are kind of syntactically unattractive
+
+
+
+
+
+
+
+
+
+while // USED when you want a loop to repeat an unknown number of times, and possible not at all. 
+  //Use you use a while loop when you want a loop to repeat an unknown number of times, but it possibly might not run all.
+//A really common example of a while looping being used is to run the control flow for a game.
+  //You might not know how long the user is going to be playing, but you want to keep doing the same thing. You want to keep things moving at all times,
+  //but you don't know when the user is going to stop playing or when they're going to run out of lives. So a while loop is really good for something like that.
+
+
+
+  do while // USED when want a loop to repeat an unknown number of times, but at least oncce. 
+  //You want a loop to repeat an unknown number of times, but at least once. You might use this for a game too, but a really common use case
+  //is prompting the user for input. 
+
+  for //USED when you want a loop to repeat a discrete number of times though you may not know the number at the moment the program is compiled.
